7d45ac89bc449951731c662210294b739aaf025f
Gunter <vlasovdenisochka@yandex.ru>
2024-11-15 17:46:15 +0500

    working commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+void Func1()
+{
+
+}
+
+void Func2()
+{
+
+}
+
+void Func3()
+{
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    vector<string> my_vector;
+    Func1();
+    Func2();
+    Func3();
 }
