d94ff6ce1533c20620f459f6c986de4d14c44fbe
Gunter <vlasovdenisochka@yandex.ru>
2024-11-15 17:42:21 +0500

    working commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-void Func1()
+void Func1(vector<string> my_vector)
 {
-
+    ifstream in;
+    in.open("text.txt");
+    if (in.is_open())
+    {
+        while (!in.eof())
+        {
+            string s;
+            in >> s;
+            my_vector.push_back(s);
+        }
+    }
+    /*for (int i = 0; i < my_vector.size(); i++)
+    {
+        cout << my_vector[i] << endl;
+    }*/
 }
 
 void Func2()
@@ -19,7 +36,8 @@ void Func3()
 
 int main()
 {
-    Func1();
+    vector<string> my_vector;
+    Func1(my_vector);
     Func2();
     Func3();
 }
