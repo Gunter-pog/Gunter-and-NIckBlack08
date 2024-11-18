c0c5290cd4836c8adbd84bdd32802fe41e5fc390
Konstantin <kkorotkikh08@gmail.com>
2024-11-18 16:55:46 +0500

    second function done

--- main.cpp
+++ main.cpp
@@ -9,9 +9,13 @@ void Func1()
 
 }
 
-void Func2()
+void Func2(vector<string> my_vector)
 {
-
+    my_vector = {"1", "2", "3"};
+    for (int i = 0; i < my_vector.size(); i++)
+    {
+        cout << my_vector[i] << endl;
+    }
 }
 
 void Func3()
@@ -23,6 +27,6 @@ int main()
 {
     vector<string> my_vector;
     Func1();
-    Func2();
+    Func2(my_vector);
     Func3();
 }
