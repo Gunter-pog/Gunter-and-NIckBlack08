e868d1348756e4adde9e55cd23e86bfd49c5c0c4
Konstantin <kkorotkikh08@gmail.com>
2024-11-18 17:49:11 +0500

    second fuction

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -9,7 +10,7 @@ void Func1()
 
 }
 
-void Func2(vector<string> my_vector)
+void Func2(vector<string> &my_vector)
 {
     my_vector = {"1", "2", "3"};
     for (int i = 0; i < my_vector.size(); i++)
@@ -18,9 +19,17 @@ void Func2(vector<string> my_vector)
     }
 }
 
-void Func3()
+void Func3(vector<string> &my_vector)
 {
-
+    ofstream outfile;
+    outfile.open("vector.txt");
+    if (outfile.is_open())
+    {
+        for (int i = 0; i < my_vector.size(); i++)
+        {
+            outfile << my_vector[i] << endl;
+        }
+    }
 }
 
 int main()
@@ -28,5 +37,5 @@ int main()
     vector<string> my_vector;
     Func1();
     Func2(my_vector);
-    Func3();
+    Func3(my_vector);
 }
